51301b01808744ba940093406e5839006befd5f5
TheoBordes <theo.bordes.parcoursup@gmail.com>
2024-11-14 23:14:53 +0100

    v2

---
+++ Robot_Minh_Theo.X/CB_TX1.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   CB_TX1.h
+ * Author: pixel
+ *
+ * Created on 14 novembre 2024, 21:09
+ */
+
+#ifndef CB_TX1_H
+#define	CB_TX1_H
+
+int CB_TX1_GetRemainingSize(void);
+int CB_TX1_GetDataSize(void);
+unsigned char CB_TX1_IsTranmitting(void);
+void SendOne();
+unsigned char CB_TX1_Get(void);
+void CB_TX1_Add(unsigned char value);
+void SendMessage(unsigned char* message, int length);
+
+
+
+#endif	/* CB_TX1_H */
+
